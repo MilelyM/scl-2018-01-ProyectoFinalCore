ace0999573b1a356845e7f532d9f5c7bf74dcecb
milelym <milelymarcial@gmail.com>
2018-07-28 01:08:26 -0400

    se crea funcion para mostrar la tabla prueba

--- inicio.js
+++ inicio.js
@@ -1,4 +1,3 @@
-
 firebase.initializeApp({
     apiKey: "AIzaSyBPfDrkFKG7TPV7UE0oaNPwyrK2qPz3728",
     authDomain: "registrovisitantes-356b2.firebaseapp.com",
@@ -6,30 +5,53 @@ firebase.initializeApp({
   });
   //Inicializa una instancia de Cloud Firestore
   var db = firebase.firestore();
+
 // se crea funcion para guardar los datos
 function guardar(){
     const rut = document.getElementById('rut').value;
-    const nombre = document.getElementById('nombre').value;
-    const apellido = document.getElementById('apellido').value;
-    const departamento = document.getElementById('departamento').value;
+    const nombreCompleto = document.getElementById('nombreCompleto').value;
+    const email = document.getElementById('email').value; // buscar funcion y hora
+    const departamento = document.getElementById('seleccion').value;// cambiar por una lista de seleccion
     const patente = document.getElementById('patente').value;
+    const credencial = document.getElementById('credencial').value;
 db.collection("visitantes").add({// agrega un id automatico  a nuestro documento
 rut: rut,
-nombre: nombre,
-apellido:apellido,
+nombreCompleto: nombreCompleto,
+email:email,
 departamento: departamento,
-patente: patente
+patente: patente,
+credencial: credencial
 
 })
 .then(function(docRef) {
     console.log("Document written with ID: ", docRef.id);
     document.getElementById('rut').value = '';
-    document.getElementById('nombre').value  = '';
-    document.getElementById('apellido').value  = '';
-    document.getElementById('departamento').value  = '';
+    document.getElementById('nombreCompleto').value  = '';
+    document.getElementById('email').value  = '';
+    document.getElementById('seleccion').value  = '';
     document.getElementById('patente').value  = '';
+    document.getElementById('credencial').value  = '';
 })
 .catch(function(error) {
     console.error("Error adding document: ", error);
 });
 }
+// leer los documentos
+var tableAdm = document.getElementById('tableAdm');
+// repite por cada documento dentro de visi y va a pintar id
+db.collection("visitantes").get().then((querySnapshot) => {
+    // limpiamos la tabla
+    tableAdm.innerHTML ='';
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+        tableAdm.innerHTML +=` 
+        <tr>
+        <td>${doc.data().rut}</td>
+        <td>${doc.data().nombreCompleto}</td>
+        <td>${doc.data().email}</td>
+        <td>${doc.data().departamento}</td>
+        <td>${doc.data().patente}</td>
+        <td>${doc.data().credencial}</td>
+      </tr>`
+    });
+});
