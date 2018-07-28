82d4f1a30d40353a40e1d72b6c2bb15009532aba
milelym <milelymarcial@gmail.com>
2018-07-27 21:22:46 -0400

    se crea funcion para guardar datos

--- inicio.js
+++ inicio.js
@@ -7,3 +7,29 @@ firebase.initializeApp({
   //Inicializa una instancia de Cloud Firestore
   var db = firebase.firestore();
 // se crea funcion para guardar los datos
+function guardar(){
+    const rut = document.getElementById('rut').value;
+    const nombre = document.getElementById('nombre').value;
+    const apellido = document.getElementById('apellido').value;
+    const departamento = document.getElementById('departamento').value;
+    const patente = document.getElementById('patente').value;
+db.collection("visitantes").add({// agrega un id automatico  a nuestro documento
+rut: rut,
+nombre: nombre,
+apellido:apellido,
+departamento: departamento,
+patente: patente
+
+})
+.then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+    document.getElementById('rut').value = '';
+    document.getElementById('nombre').value  = '';
+    document.getElementById('apellido').value  = '';
+    document.getElementById('departamento').value  = '';
+    document.getElementById('patente').value  = '';
+})
+.catch(function(error) {
+    console.error("Error adding document: ", error);
+});
+}
