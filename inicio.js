0f10efe3906a2601f714b3979c3c9a0f4762373d
milelym <milelymarcial@gmail.com>
2018-07-27 20:11:55 -0400

    se crea funcion para iniciarlizar con firestore y se agregan los input

--- inicio.js
+++ inicio.js
@@ -1,10 +1,9 @@
 
-    fetch('https://api.rutify.cl/rut/253310839')
-    .then(function(resp) {
-        return resp.json();
-        //retorna la data
-    })
-    .then(function (valores) {
-       console.log(valores)
-
-    });
+firebase.initializeApp({
+    apiKey: "AIzaSyBPfDrkFKG7TPV7UE0oaNPwyrK2qPz3728",
+    authDomain: "registrovisitantes-356b2.firebaseapp.com",
+    projectId: "registrovisitantes-356b2"
+  });
+  //Inicializa una instancia de Cloud Firestore
+  var db = firebase.firestore();
+// se crea funcion para guardar los datos
